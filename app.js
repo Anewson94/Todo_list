910a2622b2a1255ad242126df93f1612b6cdf14c
Anewson94 <dnewson59@gmail.com>
2022-11-28 13:54:11 -0500

    Intalled ejs and added ejs file views

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+"Express"
 const express = require("express")
 const app = express()
 
+"Body Parser"
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}))
+
+"EJS"
+const ejs = require("ejs")
+app.set("view engine", "ejs")
 
 const port = 3000
 
 
 app.get("/", function(req, res) {
-    res.send("/index.html")
+    
+    let today = new Date()
+    let todayDate = today.getDay()
+    let currDay = today.getDate()
+
+    let day = ""
+
+    if (todayDate <= 5) {
+        day = "weekday"
+    } else {
+        day = "weekend"
+    }  
+
+    res.render("list", {kindOfDay: day})
 })
 
-app.listen(port, function(res, req) {
+app.listen(port, function() {
     console.log("Listening on port " + port)
 })
