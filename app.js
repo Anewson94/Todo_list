06dea50546c085027790a989b48be8b61355c771
Anewson94 <dnewson59@gmail.com>
2022-12-05 18:16:22 -0500

    added CSS Sheet and updated server with post script for the to do list

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(express.static("public"))
+
 "EJS"
 const ejs = require("ejs")
 app.set("view engine", "ejs")
@@ -13,21 +15,25 @@ app.set("view engine", "ejs")
 const port = 3000
 
 
+let items = []
+
 app.get("/", function(req, res) {
-    
+    let dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
     let today = new Date()
-    let todayDate = today.getDay()
-    let currDay = today.getDate()
+    let day = today.toLocaleDateString("en-US", dateOptions)
+    console.log(day)
+    let item = req.body.newItem
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    })
 
-    let day = ""
+})
 
-    if (todayDate <= 5) {
-        day = "weekday"
-    } else {
-        day = "weekend"
-    }  
-
-    res.render("list", {kindOfDay: day})
+app.post("/", function(req, res) {
+    let item = req.body.newItem
+    items.push(item)
+    res.redirect("/")
 })
 
 app.listen(port, function() {
