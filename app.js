fc1b477398c4cb3c31a5b4b520f23433226ee238
Anewson94 <dnewson59@gmail.com>
2022-12-06 08:59:25 -0500

    Formatted code and finished to do list

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-"Express"
-const express = require("express")
-const app = express()
+"Express";
+const express = require("express");
+const app = express();
 
-"Body Parser"
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({extended: true}))
+("Body Parser");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-"EJS"
-const ejs = require("ejs")
-app.set("view engine", "ejs")
+("EJS");
+const ejs = require("ejs");
+app.set("view engine", "ejs");
 
-const port = 3000
+const port = 3000;
 
+let items = [];
 
-let items = []
+app.get("/", function (req, res) {
+  let dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let today = new Date();
+  let day = today.toLocaleDateString("en-US", dateOptions);
+  // console.log(day)
+  let item = req.body.newItem;
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items,
+  });
+});
 
-app.get("/", function(req, res) {
-    let dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
-    let today = new Date()
-    let day = today.toLocaleDateString("en-US", dateOptions)
-    console.log(day)
-    let item = req.body.newItem
-    res.render("list", {
-        kindOfDay: day,
-        newListItems: items
-    })
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 
-})
-
-app.post("/", function(req, res) {
-    let item = req.body.newItem
-    items.push(item)
-    res.redirect("/")
-})
-
-app.listen(port, function() {
-    console.log("Listening on port " + port)
-})
+app.listen(port, function () {
+  console.log("Listening on port " + port);
+});
