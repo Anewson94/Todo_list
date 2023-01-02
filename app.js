b575b3dec4383553deb55a00d3faccd23994a03e
Anewson94 <dnewson59@gmail.com>
2023-01-02 16:09:22 -0500

    added mongoose and database

--- app.js
+++ app.js
@@ -1,64 +1,97 @@
-"Express";
+// "Express";
 const express = require("express");
 const app = express();
 
-("Body Parser");
+// ("Body Parser");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname +"/public"));
 
-("EJS");
+// ("EJS");
 const ejs = require("ejs");
 app.set("view engine", "ejs");
 
+// Mongoose database
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', false);
+main().catch(err => console.log(err))
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/todolistDB')
+  
+}
+// Mongoose
+const todolistSchema = new mongoose.Schema({
+  name: String,
+})
+
+const Item = mongoose.model("Item",todolistSchema )
+
+  const item1 = new Item({
+    name: "Welcome to your To-do list."
+  })
+  const item2 = new Item({
+    name: "Hit the + button to add new item to the list."
+  })
+  const item3 = new Item({
+    name: "<-- press check to delete item."
+  })
+
+const defaultItems = [item1, item2, item3]
+
+// Item.insertMany(defaultItems, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else console.log("Succesfully added Items")
+// });
+
+
+
+// Mongoose
+
 const port = 3000;
 
-let items = [];
-let workItems = []
 
-const date = require(__dirname + "/date.js")
-let day = date()
+const date = require(__dirname + "/date.js");
+let day = date();
+let typeList = "List"
 
 app.get("/", function (req, res) {
+  Item.find({}, function(err, foundItems) {
+    
+    res.render("list", {
+      kindOfDay: day,
+      typeList: typeList,
+      newListItems: foundItems,
+    });
+  })
   // console.log(day)
   let item = req.body.newItem;
-  res.render("list", {
-    kindOfDay: day,
-    typeList: day,
-    newListItems: items,
-  });
+  
 });
 
 
 app.post("/", function (req, res) {
     let item = req.body.newItem;
     console.log(req.body)
-    if (req.body.list == "Work List") {
-        workItems.push(item)
-        res.redirect("/work")
-    } else {
-        items.push(item);
-        res.redirect("/");
-     }
-
-  
+    
+      
 });
 
 app.post("/work", function(req, res) {
     let item = req.body.newItem
-    workItems.push(item)
+    
     res.redirect("/work")
     
 })
 
 app.get("/work", function(req,res) {
     res.render("list", {kindOfDay: day, typeList: "Work List", newListItems: workItems})
-})
+});
 
 app.get("/about", function(req, res) {
     res.render("about")
-})
+});
 
 app.listen(port, function () {
   console.log("Listening on port " + port);
